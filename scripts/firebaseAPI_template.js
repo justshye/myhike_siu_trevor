dcc31c4629f821b435c6b45ca47a74ea9d5598a7
shye <113232835+justshye@users.noreply.github.com>
2022-10-25 11:49:14 -0700

    finished lecture instructions

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyDtPH1MooktCNzCP8x1cuh5t3HmZOY0EcA",
+    authDomain: "comp1800-202230-fd127.firebaseapp.com",
+    projectId: "comp1800-202230-fd127",
+    storageBucket: "comp1800-202230-fd127.appspot.com",
+    messagingSenderId: "222906377832",
+    appId: "1:222906377832:web:84f8316683fe9cf2b6fddd"
 };
 
 //--------------------------------------------
